d9f625033512f39b3907f846f810eda0250d5ee3
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-09 00:18:05 -0300

    ✏️ build com add client layout

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 // vite-env.d.ts
 /// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>

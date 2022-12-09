b03b88ce46aeda5c06a43f5477ebcdbf72d7f862
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-08 23:49:27 -0300

    💚 fixed build

--- src/views/about/me/[id].vue
+++ src/views/about/me/[id].vue
@@ -1,10 +1,23 @@
 <template>
   <div>
     <h1>{{ id }}</h1>
-    <Title />
-
+    <q-btn @click="ping">Ping</q-btn>
   </div>
 </template>
 <script setup lang="ts">
+import { useQuery, useClient, useMutation } from 'villus'
 defineProps<{id: string}>()
+const query = `query Ping {
+  ping
+}`
+
+useClient({
+  url: 'http://localhost:4001'
+});
+
+
+await useQuery({query})
+async function ping() {
+  // getEnvironmentVariable('GQL_ENDPOINT')
+}
 </script>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
-
+// vite-env.d.ts
+/// <reference types="vite-plugin-pages/client" />
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>

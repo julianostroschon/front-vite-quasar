b525411ee00f32b686164d602279093df710ca56
whoistaiza <taizamoura2016@gmail.com>
2022-12-12 18:46:17 -0300

    ✨ componentizing item of drawer

---
+++ src/components/NavBar/Item.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-expansion-item
+    expand-separator
+    :icon="item.icon"
+    :label="item.label"
+    default-opened
+  >
+    <q-item clickable v-ripple v-for="subItem in item.subItems" :key="subItem">
+      <q-item-section> {{ subItem.name }} </q-item-section>
+    </q-item>
+  </q-expansion-item>
+
+</template>
+
+<script setup lang="ts">
+defineProps({
+  item: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>

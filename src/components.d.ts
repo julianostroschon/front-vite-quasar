f5e98aa94a3a49d2be3a672d7fd37b18e058e590
whoistaiza <taizamoura2016@gmail.com>
2022-12-13 10:15:32 -0300

    🚚 rename components Header and Layout

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,15 +9,17 @@ declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     ButtonHome: typeof import("./components/ButtonHome.vue")["default"];
     Header: typeof import("./components/Header/index.vue")["default"];
+    HeaderBar: typeof import("./components/HeaderBar/index.vue")["default"];
     HeaderNav: typeof import("./components/HeaderNav.vue")["default"];
     HelloWorld: typeof import("./components/HelloWorld.vue")["default"];
     Item: typeof import("./components/SideBar/Item.vue")["default"];
     Layout: typeof import("./components/Layout/index.vue")["default"];
+    MainLayout: typeof import("./components/MainLayout/index.vue")["default"];
     MakdownEditor: typeof import("./components/MakdownEditor.vue")["default"];
     RouterLink: typeof import("vue-router")["RouterLink"];
     RouterView: typeof import("vue-router")["RouterView"];
     SideBar: typeof import("./components/SideBar/index.vue")["default"];
     SubItem: typeof import("./components/SideBar/SubItem.vue")["default"];
-    ToolBar: typeof import("./components/Header/ToolBar.vue")["default"];
+    ToolBar: typeof import("./components/HeaderBar/ToolBar.vue")["default"];
   }
 }

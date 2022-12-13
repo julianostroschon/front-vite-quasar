487e75299dcad9d707412f345f9d3f5481be1279
whoistaiza <taizamoura2016@gmail.com>
2022-12-13 09:38:52 -0300

    🚚 move header to own folder

---
+++ src/components/Header/ToolBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-toolbar>
+    <q-btn flat @click="$emit('openMenu')" round dense icon="menu" />
+    <q-toolbar-title> Header </q-toolbar-title>
+  </q-toolbar>
+</template>
+
+<script setup lang="ts">
+defineEmits(["openMenu"]);
+</script>

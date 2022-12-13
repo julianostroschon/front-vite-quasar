8cfb6e68a0a3d1adfc6c9092aedb89759e65b715
whoistaiza <taizamoura2016@gmail.com>
2022-12-13 14:27:41 -0300

    ✨ componentizing header item of menu list

--- src/components/SideBar/ItemList.vue
+++ src/components/SideBar/ItemList.vue
@@ -1,10 +1,8 @@
 <template>
-  <q-expansion-item
-    expand-separator
-    :icon="item.icon"
-    :label="$t(item.label)"
-    default-opened
-  >
+  <q-expansion-item hide-expand-icon class="text-left">
+    <template v-slot:header>
+      <HeaderItem v-bind="$props" />
+    </template>
     <SubItem
       v-for="subItem in item.subItems"
       :key="subItem"

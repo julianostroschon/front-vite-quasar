18ecae38cd7aee1c1f47e9b48eebad1871cb39fc
whoistaiza <taizamoura2016@gmail.com>
2022-12-14 09:24:32 -0300

    ✨ new component dynamic modal

---
+++ src/components/DynamicDialog/CardFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-card-actions align="right">
+    <q-btn flat label="OK" color="primary" v-close-popup />
+    <q-btn flat label="Cancelar" color="primary" v-close-popup />
+  </q-card-actions>
+</template>
---
+++ src/components/DynamicDialog/MainCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-card class="card-width">
+    <CardHeader v-bind="$props" />
+    <q-card-section>
+      <slot />
+    </q-card-section>
+    <CardFooter />
+  </q-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+});
+</script>
+
+<style scoped>
+.card-width {
+  width: 30vw;
+}
+</style>

8834d575fa1d23dd1d54910b5319b123177b5832
whoistaiza <taizamoura2016@gmail.com>
2022-12-15 09:01:35 -0300

    ✨ form of login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h2>Login</h2>
+  <q-card class="card-size absolute-center" flat bordered>
+    <q-card-section>
+      <div class="text-h3">{{ $t("login.title") }}</div>
+    </q-card-section>
+    <q-card-section>
+      <LoginForm />
+    </q-card-section>
+  </q-card>
 </template>
+
+<script setup lang="ts">
+import LoginForm from "./LoginForm.vue";
+</script>
+
+<style scoped>
+.card-size {
+  width: 30vw;
+  height: auto;
+}
+</style>

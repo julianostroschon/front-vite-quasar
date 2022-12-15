444d5769b9a4d26a07b99eb2096122d5bd7d422c
whoistaiza <taizamoura2016@gmail.com>
2022-12-15 08:52:56 -0300

    ✨ add login words in translation lib

--- locales/br/index.ts
+++ locales/br/index.ts
@@ -1,9 +1,11 @@
-import caderno from './caderno';
-import markdown from './markdown';
+import caderno from "./caderno";
+import markdown from "./markdown";
+import login from "./login";
 
 export default {
-  about: 'Sobre',
-  tags: 'Entiquetas',
+  about: "Sobre",
+  tags: "Entiquetas",
   caderno,
   markdown,
+  login,
 };
---
+++ locales/br/login.ts
@@ -0,0 +1,9 @@
+export default {
+  title: "Login",
+  labelInputName: "Seu nome de usuário",
+  labelInputPassword: "Sua senha",
+  hintName: "Sua matrícula",
+  hintPassword: "6 primeiros números do seu cpf",
+  submitButton: "Enviar",
+  warning: "Por favor digite algo!",
+};

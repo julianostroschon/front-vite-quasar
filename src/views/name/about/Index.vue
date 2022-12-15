e15a27f3f766314513e706cd179220784726b9f0
whoistaiza <taizamoura2016@gmail.com>
2022-12-15 13:56:25 -0300

    🔥 remove files

--- src/views/name/about/Index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>{{$t('about')}}</h1>
-  <HelloWorld :msg="name" />
-  <HeaderNav />
-</template>
-<script setup lang="ts">
-defineProps<{ name: string }>()
-
-</script>
--- src/views/news/Index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>Another World</h1>
-  <h6>newsPage</h6>
-  <HelloWorld msg="Se lascou" />
-</template>

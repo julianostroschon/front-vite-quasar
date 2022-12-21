92198d3ab88b5d5e613331d34468dce17da86bbf
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-21 18:55:41 -0300

    🏗️ alterado local dos injections

--- vite.config.ts
+++ vite.config.ts
@@ -1,67 +1,58 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import VueI18n from '@intlify/vite-plugin-vue-i18n';
-import AutoImport from 'unplugin-auto-import/vite';
-import path from 'node:path';
-import Pages from 'vite-plugin-pages';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
-
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import VueI18n from "@intlify/vite-plugin-vue-i18n";
+import AutoImport from "unplugin-auto-import/vite";
+import path from "node:path";
+import Pages from "vite-plugin-pages";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '~': `${path.resolve(__dirname, 'src')}/`,
+      "~": `${path.resolve(__dirname, "src")}/`,
     },
   },
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
 
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
+      sassVariables: "src/quasar-variables.sass",
     }),
     Pages({
       // Here we can add 'md'
-      extensions: ['vue'],
-      dirs: [
-        { dir: 'src/views', baseRoute: '' },
-      ]
+      extensions: ["vue"],
+      dirs: [{ dir: "src/views", baseRoute: "" }],
     }),
     AutoImport({
       imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        'vue/macros',
-        '@vueuse/head',
-        '@vueuse/core',
+        "vue",
+        "vue-router",
+        "vue-i18n",
+        "vue/macros",
+        "@vueuse/head",
+        "@vueuse/core",
       ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/composables',
-        'src/store',
-        'src/helpers',
-        'src/stores'
-      ],
+      dts: "src/injection/auto-imports.d.ts",
+      dirs: ["src/composables", "src/store", "src/helpers", "src/stores"],
       vueTemplate: true,
     }),
-    
+
     Components({
       // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
+      dts: "src/injection/components.d.ts",
     }),
 
     VueI18n({
       runtimeOnly: true,
       compositionOnly: true,
-      include: [ path.resolve(__dirname, './locales/**') ],
+      include: [path.resolve(__dirname, "./locales/**")],
     }),
-  ]
+  ],
 });

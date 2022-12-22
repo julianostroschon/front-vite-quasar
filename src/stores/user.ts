6af4e94ba59cba10b58775bae19815f13c38fc91
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-22 13:02:47 -0300

    🐛 add importação ref na store de user

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,27 +1,26 @@
-import { defineStore, acceptHMRUpdate } from 'pinia';
+import { defineStore, acceptHMRUpdate } from "pinia";
+import { ref } from "vue";
 
-export const useUserStore = defineStore('user', () => {
-
+export const useUserStore = defineStore("user", () => {
   /**
    * Current name of the user.
    */
-  const username = ref('Juliano');
+  const username = ref("Juliano");
   const age = ref(29);
-  
+
   /**
    * `incrementAge` is a function that takes no arguments and returns nothing
    */
   function incrementAge() {
     age.value += 1;
   }
- 
+
   return {
     username,
     age,
-    incrementAge
+    incrementAge,
   };
 });
- 
+
 if (import.meta.hot)
   import.meta.hot.accept(acceptHMRUpdate(useUserStore, import.meta.hot));
- 

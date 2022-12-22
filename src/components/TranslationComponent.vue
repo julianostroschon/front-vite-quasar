7fa4de3aa3b6742c16767d56bce0116452bd0f8e
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-22 08:38:31 -0300

    🚩 fabButton e TranslationComponent funcionando

---
+++ src/components/TranslationComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const t = useI18n();
+
+defineExpose({
+  toggleLocale: () => {
+    const locales = t.availableLocales;
+    const fromLocale = t.locale.value;
+    t.locale.value =
+      locales[(locales.indexOf(t.locale.value) + 1) % locales.length];
+    console.warn(fromLocale, " -> ", t.locale.value);
+  },
+});
+</script>
+<template>
+  <i class="material-icons">translate</i>
+</template>

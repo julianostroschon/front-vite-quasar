2741e491bb0916aad55325001a5cecfe4fe55be8
Juliano Stroschon <julianostroschon@gmail.com>
2023-01-16 08:03:06 -0300

    ➕ add interpretador e loader graphql

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+declare module "*.gql" {
+  const content: any;
+  export default content;
 }

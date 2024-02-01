18fe7c45d46d0d5d67013e5d2975f37b8030f60e
julianostroschon <julianostroschon@gmail.com>
2024-02-01 13:04:16 -0300

    🎉 Remoção arquivos desnecessários

--- src/modules/villus.ts
+++ src/modules/villus.ts
@@ -1,7 +1,6 @@
 import { createClient, defaultPlugins } from "villus";
 import { getEnvironmentVariable } from "../helpers";
 
-// Creates a villus client instance
 const url = getEnvironmentVariable("VITE_APP_ENDPOINT");
 export const villus = createClient({
   use: [...defaultPlugins()],

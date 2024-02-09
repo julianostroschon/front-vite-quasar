5378b65a071392410cba1253792dd9233da199e0
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-09 15:02:14 -0300

    🐛 Facilitando o entendimento do código

--- src/components/TabHeader/BarUp.vue
+++ src/components/TabHeader/BarUp.vue
@@ -1,14 +1,12 @@
 <template>
   <q-tabs
     v-model="tab"
-    class="tab-header text-white fixed-top"
+    class="tab-header z-top text-white fixed-top ellipsis"
     :style="{ backgroundColor: tabBackgroundColor }"
   >
     <q-item clickable to="/home" class="q-mx-md q-mt-sm">
-      <q-toolbar-title class="q-pa-md">
-        <q-avatar square class="ico bg-transparent">
-          <img src="/logo_fundimisa.png" />
-        </q-avatar>
+      <q-toolbar-title class="q-px-md">
+        <img src="/logo_fundimisa.png" />
       </q-toolbar-title>
     </q-item>
     <q-space />
@@ -19,7 +17,7 @@
         :key="item.name"
         :name="item.name"
         :label="item.label"
-        :class="{ 'q-tab-active text-h4': tab === item.name }"
+        class="text-bold"
       />
     </q-tabs>
 
@@ -27,35 +25,23 @@
     <q-btn size="xl" flat icon="settings" round class="setting_ico q-pa-md" />
   </q-tabs>
 </template>
+
 <script setup lang="ts">
 import { tabItems } from "./lib";
 const tab = ref("");
 const tabBackgroundColor = ref("rgba(57, 73, 171, 1)");
-window.addEventListener("scroll", function () {
-  const scrollVertical = window.scrollY || window.pageYOffset;
-  if (scrollVertical == 0) {
+const scrollTop = ref(0);
+function handleScroll() {
+  scrollTop.value = window.pageYOffset;
+  if (!scrollTop.value) {
     return (tabBackgroundColor.value = "rgba(57, 73, 171, 1)");
   }
   return (tabBackgroundColor.value = "rgba(63, 81, 181, 0.8)");
+}
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+watchEffect(() => {
+  handleScroll;
 });
 </script>
-<style scoped>
-.ico {
-  width: 100%;
-}
-
-.q-tab-active {
-  background-color: white;
-  color: rgb(47, 47, 145);
-  border-radius: 10px;
-}
-
-.font-tabs {
-  font-family: "Fira Sans", sans-serif;
-}
-
-.tab-header {
-  width: 100%;
-  z-index: 1000;
-}
-</style>

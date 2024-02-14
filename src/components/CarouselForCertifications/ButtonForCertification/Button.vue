360e1e31e11763e29806398992d99fa66384ddb1
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-14 13:08:37 -0300

    ✨ Component for certifications

---
+++ src/components/CarouselForCertifications/ButtonForCertification/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-1 self-center">
+    <q-btn
+      flat
+      :icon="props.icoBtn"
+      class="bg-indigo-8"
+      color="white"
+      rounded
+      @click="navSlides()"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  direction: {
+    type: String,
+    required: true,
+  },
+  icoBtn: {
+    type: String,
+    default: "",
+  },
+});
+
+const emits = defineEmits(["navigation"]);
+
+function navSlides() {
+  emits("navigation", props.direction);
+}
+</script>
---
+++ src/components/CarouselForCertifications/lib.ts
@@ -0,0 +1,6 @@
+export const imgs = [
+  { title: "ISO-9001", img: "ISO9001.jpeg" },
+  { title: "ISO-9002", img: "ISO9001.jpeg" },
+  { title: "ISO-9003", img: "ISO9001.jpeg" },
+  { title: "ISO-9004", img: "ISO9001.jpeg" },
+];

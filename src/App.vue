3849686a9616419c5089777eae8177be7f105d30
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-16 08:34:25 -0300

    ✨ Criação da página RH_DIVULGA

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
+  <BarUp v-if="showTabHeader" />
+
   <RouterView />
 </template>
+<script setup lang="ts">
+const exceptionRoutes = ["/login"];
+const showTabHeader = computed(() => {
+  const currentRoute = useRoute();
+  return !exceptionRoutes.some((route) => currentRoute.path.includes(route));
+});
+</script>
